5ff155af36fd709733f98b1b7f75adda5103d7c3
divine <divineobi07@gmail.com>
2022-07-29 12:37:43 +0100

    add modal component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,63 @@
+import React, { createPortal } from "react-dom";
 import "./Header.css";
 import Logo from "./Logo";
 import Hamburger from "../../assets/icon-hamburger.svg";
+import close_btn from "../../assets/icon-close.svg";
 import Lg from "../../assets/illustration-intro.svg";
 import PatternImg from "../../assets/bg-tablet-pattern.svg";
 import DesktopHeader from "./DesktopHeader";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const Header = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const toggleHandler = () => {
+    setClicked((state) => !state);
+  };
+
+  const classes = clicked ? close_btn : Hamburger
+
+
   return (
-    <div className="header_wrapper">
-      <header className="header">
-        <div className="header_box">
-          <Logo />
-          <button className="nav_button">
-            <img src={Hamburger} alt="" />
-          </button>
+    <>
+      {clicked && createPortal(<Modal />, document.querySelector(".modal"))}
+      <div className="header_wrapper">
+        <header className="header">
+          <div className="header_box">
+            <Logo />
+            <button className="nav_button" onClick={toggleHandler}>
+              <img src={classes} alt="" />
+            </button>
 
-          <DesktopHeader />
-        </div>
-
-        <section className="header_main">
-          <figure className="header_illustrations">
-            <img src={PatternImg} alt="" className="other" />
-            <img src={Lg} alt="" className="main_ill" />
-          </figure>
-          <div className="header_content">
-            <h1 className="header_title">
-              <span>Bring everyone</span>
-              <span> together to build</span>
-              <span>better products.</span>
-            </h1>
-            <p className="header_detail">
-              Manage makes it simple for software teams to plan day-to-day tasks
-              while keeping the larger team goals in view.
-            </p>
-
-            <button className="header_btn">Get Started</button>
+            <DesktopHeader />
           </div>
-          <div className="class">
-            <img src={PatternImg} alt="" className="other3" />
-          </div>
-        </section>
-      </header>
-    </div>
+
+          <section className="header_main">
+            <figure className="header_illustrations">
+              <img src={PatternImg} alt="" className="other" />
+              <img src={Lg} alt="" className="main_ill" />
+            </figure>
+            <div className="header_content">
+              <h1 className="header_title">
+                <span>Bring everyone</span>
+                <span> together to build</span>
+                <span>better products.</span>
+              </h1>
+              <p className="header_detail">
+                Manage makes it simple for software teams to plan day-to-day
+                tasks while keeping the larger team goals in view.
+              </p>
+
+              <button className="header_btn">Get Started</button>
+            </div>
+            <div className="class">
+              <img src={PatternImg} alt="" className="other3" />
+            </div>
+          </section>
+        </header>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Header/Modal.js
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "./Modal.css";
+const Modal = () => {
+  return (
+    <nav className="modal_container">
+      <a href="#">Pricing</a>
+      <a href="#">Product</a>
+      <a href="#">About Us</a>
+      <a href="#">Careers</a>
+      <a href="#">Community</a>
+    </nav>
+  );
+};
+
+export default Modal;
